d14b3da9a2cb49e8f702d458bbee94439b340801
Adrian <klimasa98@gmail.com>
2024-04-20 19:45:40 +0200

    finish projects section for mobile

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.navigation');
-const blur = document.querySelector('.blur')
+const blur = document.querySelector('.blur');
+const main = document.querySelector('main');
 
 const handleClick = () => {
 	hamburger.classList.toggle('hamburger--active');
-   nav.classList.toggle('navigation--active');
-   blur.classList.toggle('blur--active')
+	nav.classList.toggle('navigation--active');
+	blur.classList.toggle('blur--active');
+	main.style.filter = blur('3');
 };
 
 hamburger.addEventListener('click', handleClick);
