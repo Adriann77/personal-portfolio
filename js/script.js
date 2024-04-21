b6222d4cf0e643b491e8fedac7451ead505bb690
Adrian <klimasa98@gmail.com>
2024-04-21 04:24:23 +0200

    change mobile view for projects section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.navigation');
-const blur = document.querySelector('.blur');
-const main = document.querySelector('main');
+const navLinks = document.querySelectorAll('.navigation__item');
+const navDesktop = document.querySelector('.nav-desktop')
 
 const handleClick = () => {
 	hamburger.classList.toggle('hamburger--active');
@@ -10,4 +10,15 @@ const handleClick = () => {
 	main.style.filter = blur('3');
 };
 
+navLinks.forEach(link => {
+	link.addEventListener('click', () => {
+		nav.classList.remove('navigation--active');
+		hamburger.classList.remove('hamburger--active');
+	});
+});
+
 hamburger.addEventListener('click', handleClick);
+
+document.body.addEventListener('scroll', () => {
+   navDesktop.classList.add('nav-desktop-onScroll')
+})
