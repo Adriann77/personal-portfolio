f95e06543c717bb417090dd77c80f8175a802f6a
Adrian <klimasa98@gmail.com>
2024-04-22 05:56:33 +0200

    fix some css & add blur

--- js/script.js
+++ js/script.js
@@ -2,23 +2,38 @@ const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.navigation');
 const navLinks = document.querySelectorAll('.navigation__item');
 const navDesktop = document.querySelector('.nav-desktop')
+const blur = document.querySelector('.blur')
 
 const handleClick = () => {
 	hamburger.classList.toggle('hamburger--active');
-	nav.classList.toggle('navigation--active');
-	blur.classList.toggle('blur--active');
-	main.style.filter = blur('3');
+   nav.classList.toggle('navigation--active');
+   blur.classList.toggle('blur--active')
 };
 
 navLinks.forEach(link => {
-	link.addEventListener('click', () => {
-		nav.classList.remove('navigation--active');
+   link.addEventListener('click', () => {
+      nav.classList.remove('navigation--active');
 		hamburger.classList.remove('hamburger--active');
+      blur.classList.remove('blur--active')
 	});
 });
 
 hamburger.addEventListener('click', handleClick);
 
-document.body.addEventListener('scroll', () => {
-   navDesktop.classList.add('nav-desktop-onScroll')
+let currScrollY = 0
+
+document.addEventListener('scroll', () => {
+   if (currScrollY < scrollY) {
+      navDesktop.style.transition = 'all .3s'
+      navDesktop.style.top = '-100px'
+      currScrollY = scrollY
+   } else {
+      navDesktop.style.top = '0px'
+      currScrollY = scrollY
+      
+   }
 })
+
+
+
+
